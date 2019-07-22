0ee1f03165d145737e11cf6d9df4e2a236653e87
Jen <jenstreetman@gmail.com>
2019-07-22 10:37:20 -0600

    Update front page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   <div class="row">
       <div class="col-3 p-5">
-        <img src="https://i.pinimg.com/564x/d3/fb/0c/d3fb0c30a77046556e81d923b2ee3707.jpg" class="rounded-circle" height="150px">
+        <img src="https://i.pinimg.com/564x/d3/fb/0c/d3fb0c30a77046556e81d923b2ee3707.jpg" class="rounded-circle" width="100%">
       </div>
       <div class="col-9 pt-5 pl-9">
         <div><h1>freecodecamp</h1></div>
@@ -13,9 +13,16 @@
             <div class="pr-5"><strong>23k</strong> followers</div>
             <div class="pr-5"><strong>212</strong> following</div>
         </div>
-      
-      </div>
+        <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+        <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+        <div><a href="https://www.freecodecamp.org/">freecodecamp.org</a> </div>
+      </div>      
   </div>
+  <div class="row pt-5">
+      <div class="col"><img src="https://scontent-den4-1.cdninstagram.com/vp/7672b8e4a45d3b2331d5ec9b362d7b27/5DAF3342/t51.2885-15/e35/66096029_362133041137173_4364216796366985117_n.jpg?_nc_ht=scontent-den4-1.cdninstagram.com" class="w-100"></div>
+      <div class="col"><img src="https://scontent-den4-1.cdninstagram.com/vp/7672b8e4a45d3b2331d5ec9b362d7b27/5DAF3342/t51.2885-15/e35/66096029_362133041137173_4364216796366985117_n.jpg?_nc_ht=scontent-den4-1.cdninstagram.com" class="w-100"></div>
+      <div class="col"><img src="https://scontent-den4-1.cdninstagram.com/vp/7672b8e4a45d3b2331d5ec9b362d7b27/5DAF3342/t51.2885-15/e35/66096029_362133041137173_4364216796366985117_n.jpg?_nc_ht=scontent-den4-1.cdninstagram.com" class="w-100"></div>
+</div>
 
 </div>
 @endsection
