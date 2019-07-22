2bd593c54e30a6f1759f63fef1ed066dc4a8b886
Jen <jenstreetman@gmail.com>
2019-07-21 23:29:38 -0600

    Initializa repo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+      <div class="col-3 p-5">
+        <img src="https://i.pinimg.com/564x/d3/fb/0c/d3fb0c30a77046556e81d923b2ee3707.jpg" class="rounded-circle" height="150px">
+      </div>
+      <div class="col-9 pt-5 pl-9">
+        <div><h1>freecodecamp</h1></div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>253</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+        </div>
+      
+      </div>
+  </div>
+
+</div>
+@endsection
